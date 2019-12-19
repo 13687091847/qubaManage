4bd1048b7ce4db35fbc3e3a7e9fe6ff28abd1320
13687091847 <357066443@qq.com>
2019-12-19 11:34:38 +0800

    ssss

---
+++ src/main/java/com/lhm/qubaManage/QubaManageApplication.java
@@ -0,0 +1,13 @@
+package com.lhm.qubaManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QubaManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QubaManageApplication.class, args);
+	}
+
+}
